4d6affdd0d0be527c2d32e939a73b18c177cd289
maram qais <maramqaiszebar@gmail.com>
2024-03-11 21:18:54 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+
+
+import React from 'react';
+import Home from './Home'; 
+import NavBar from './NavBar';
+import About from './About';
+
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Home />
+            <About />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
+import { username, city } from '../data/user'; // Importing username and city from user.js
 
 function Home() {
-  return (
-    <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to {username}'s portfolio</h1>
+            <p>Located in {city}</p>
+        </div>
+    );
 }
+
+export default Home;
